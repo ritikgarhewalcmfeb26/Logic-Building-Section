b10da00a1234f536ec15efc6f8bfc87cccf04545
Ritik Garhewal <ritik.garhewal.cmfeb26@gmail.com>
2026-02-22 23:47:44 +0530

    Add files via upload

---
+++ Assignment 4/Q23/AsteriskPattern2.java
@@ -0,0 +1,25 @@
+public class AsteriskPattern2 {
+    public static void main(String[] args) {
+        int rows = 6;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
